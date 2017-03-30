e8da41193238a7c677ec7ff8339095ec3e71be3b
Tracy Miranda <tracy@kichwacoders.com>
2017-03-30 12:43:30 +0100

    Fix formatting, show total and sort

--- track_count.py
+++ track_count.py
@@ -1,13 +1,16 @@
 from report import *
 
 def show_track_count(S):
-    print "Track Count\t\tSubmission Count"
+    print "Track Count".ljust(40) + "\t\tSubmission Count"
     
-    for (track, count) in S.track_count().items():
+    items = S.track_count().items()
+    total = sum([count for (track, count) in items])
+    for (track, count) in sorted(items, cmp=lambda (a_track, a_count), (b_track, b_count): cmp(b_count, a_count)):
         if track:
-            print "%s\t\t%s" % (track.ljust(20), count)
+            print "%s\t\t%s" % (track.ljust(40), count)
+    print "Total".ljust(40) + "\t\t%s" % total
 
 if __name__ == "__main__":
 #    S = ALL.standard().vote_cutoff(4.0)
-    S = ALL.standard().filter(lambda s: s.accepted)
+    S = ALL.standard() #.filter(lambda s: s.accepted)
     show_track_count(S)
