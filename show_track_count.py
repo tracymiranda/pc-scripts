517532a378a987502b3d5e85210f743c69497fda
Tracy Miranda <tracy@kichwacoders.com>
2017-03-30 14:12:07 +0100

    Condense the sorting code

--- show_track_count.py
+++ show_track_count.py
@@ -1,11 +1,14 @@
+from operator import itemgetter
+
 from report import *
+
 
 def show_track_count(S):
     print "Standard Track Count".ljust(40) + "\t\tSubmission Count"
     
     items = S.track_count().items()
     total = sum([count for (track, count) in items])
-    for (track, count) in sorted(items, cmp=lambda (a_track, a_count), (b_track, b_count): cmp(b_count, a_count)):
+    for (track, count) in sorted(items, key=itemgetter(1), reverse=True):
         if track:
             print "%s\t\t%s" % (track.ljust(40), count)
     print "Total".ljust(40) + "\t\t%s" % total
--- speaker_count.py
+++ speaker_count.py
@@ -1,11 +1,13 @@
+from operator import itemgetter
 from report import *
 
 def show_speaker_count(S):
-    print "Speaker Count\t\tSubmission Count"
-    
-    for (speaker, count) in S.speaker_count().items():
+    print "Speaker Count".ljust(40) + "\t\tSubmission Count"
+
+    items = S.speaker_count().items()    
+    for (speaker, count) in sorted(items, key=itemgetter(1), reverse=True):
         if count > 1:
-            print "%s\t\t%s" % (speaker.ljust(20), count)
+            print "%s\t\t%s" % (speaker.ljust(40), count)
 
 if __name__ == "__main__":
     show_speaker_count(S)
