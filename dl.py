63a0e19f481eb2a020120d6e5c44cd9532309030
Tracy Miranda <tracy@kichwacoders.com>
2017-03-30 10:23:32 +0100

    Update urls for ECF

--- dl.py
+++ dl.py
@@ -6,7 +6,7 @@ import urllib
 YEAR = 2016
 
 if __name__ == "__main__":
-    p = os.path.join(os.path.expanduser('~'), ".foss4g")
+    p = os.path.join(os.path.expanduser('~'), ".ecf2017")
     f = yaml.load(open(p, 'r'))
     usr =  urllib.quote_plus(f['user'])
     pwd =  urllib.quote_plus(f['pwd'])
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     cmd = ['wget',
            '--save-cookies', 'cookies.txt',
            '--post-data', 'name=%s&pass=%s&form_build_id=form--wyE7ecVE7aujFTM9ylmm-fIRruT1qmVuEqTDoRtGEc&form_id=user_login&op=Log+in' % (usr, pwd),
-           'https://%d.foss4g-na.org/user/login' % YEAR]
+           'https://www.eclipsecon.org/france2017/user/login' % YEAR]
 
     call(cmd)
 
@@ -23,11 +23,11 @@ if __name__ == "__main__":
     cmd = ['wget',
            '--load-cookies', 'cookies.txt',
            '-O', 'submissions.csv',
-           'https://%d.foss4g-na.org/pc-admin/sessions/eclipsecon-admin-unprocessed.csv?order=value_2&sort=asc' % YEAR]
+           'https://www.eclipsecon.org/france2017/pc-admin/sessions/eclipsecon-admin-unprocessed.csv?order=value_2&sort=asc' % YEAR]
     call(cmd)
 
     cmd = ['wget',
            '--load-cookies', 'cookies.txt',
            '-O', 'community-votes.csv',
-           'https://%d.foss4g-na.org/community-voting/report.csv' % YEAR]
+           'https://www.eclipsecon.org/france2017/community-voting/report.csv' % YEAR]
     call(cmd)
